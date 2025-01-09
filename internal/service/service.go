9c691f52184daaa70f29b36da4ad1f77cb85b7bb
Sofiya <sofiytula71@gmail.com>
2025-01-10 00:24:36 +0300

    add http

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/bulbosaur/game-of-life/pkg/life"
+)
+
+type LifeService struct {
+	currentWorld *life.World
+	nextWorld    *life.World
+}
+
+func New(height, width int) (*LifeService, error) {
+	rand.NewSource(time.Now().UTC().UnixNano())
+
+	currentWorld := life.NewWorld(height, width)
+	currentWorld.Seed()
+
+	newWorld := life.NewWorld(height, width)
+
+	ls := LifeService{
+		currentWorld: currentWorld,
+		nextWorld:    newWorld,
+	}
+
+	return &ls, nil
+}
+
+func (ls *LifeService) NewState() *life.World {
+	life.NextState(ls.currentWorld, ls.nextWorld)
+
+	ls.currentWorld = ls.nextWorld
+
+	return ls.currentWorld
+}
